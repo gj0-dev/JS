a49d28dd017fc8c755e806a41d1102f6061d984c
Garry Joss <73390442+gj0-dev@users.noreply.github.com>
2022-01-16 13:06:01 +0000

    more basics

--- Start/app.js
+++ Start/app.js
@@ -1,4 +1,23 @@
-alert("IT'S WORKING!");
+// alert("IT'S WORKING!");
 console.log(3 + 4);
 console.error('OH NO');
 console.log(3 + 4, "hello", true);
+
+if (1==1) {
+    console.log("It's true")
+}
+
+let rating = 3;
+
+if (rating === 3) {
+    console.log("YOU ARE A SUPERSTAR")
+}
+if (rating < 3) {
+    console.log("YOU ARE NOT A SUPERSTAR")
+}
+
+let num = 37;
+
+if (num % 2 !== 0) {
+    console.log("ODD NUMBER")
+}
